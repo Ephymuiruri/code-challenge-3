3a301e55c54f1ba7b08f78461ecb3ac69b7c4116
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-04-08 08:19:50 +0300

    code challenge three complete

--- src/index.js
+++ src/index.js
@@ -5,25 +5,25 @@ document.addEventListener("DOMContentLoaded",()=>{
     const runtime = document.getElementById('runtime')
     const filmInfo =document.getElementById('film-info')
     const showtime= document.getElementById('showtime')
-    const moviePoster= document.getElementById('poster')
+    const moviePoster= document.querySelector('#poster')
     const ticketNum = document.querySelector('#ticket-num')
     const remainingTickets =document.querySelector("#remaining-tickets")
     const buyTicket =document.getElementById("buy-ticket")
-    fetch(url)
+ fetch(url)
     .then(res =>res.json())
     .then((data)=>{
-        // Display movie titles in the menu
+        // Display movi
+        console.log(data)
         displayMenu(data)
-        const onLoad= data[0]
-        console.log(onLoad)
+        const onLoad = data[0]
         //Render detailed info of the first movie
         renderInfo(onLoad)
         return data
     })
-    .catch((error)=>{console.log(`error is ${error}`)})
+    .catch((error)=>{console.log(error)})
 
     function renderInfo (movieData){
-        moviePoster.setAttribute("src",`${movieData.poster}`)
+       moviePoster.setAttribute('src',`${movieData.poster}`)
        movieTitle.textContent= `${movieData.title}`
        runtime.textContent=`${movieData.runtime} minutes`
        filmInfo.textContent=`${movieData.description}`
@@ -36,36 +36,45 @@ document.addEventListener("DOMContentLoaded",()=>{
        
        buyTicket.addEventListener('click',()=>{
         
-        if (availableTickets>0){
+        if (availableTickets>=1){
         availableTickets-= 1
         remainingTickets.innerText=`${availableTickets} remaining-tickets`
         updateTickets(movieData)
-        } 
+        } else{
+           const buyButton=document.getElementById('buy-ticket')
+           buyButton.textContent='Sold Out' 
+           console.log(movieTitle.textContent)
+           document.getElementById(`${movieTitle.textContent}`).classList.add('sold-out')
+
+        }
         })
     }
     function displayMenu (movieLlist){
        movieLlist.forEach((movie) => {
        const list=document.createElement('li')
        list.className='film item'
+       list.id=`${movie.title}`
        list.innerText=`${movie.title}`
-       const sidebar=document.getElementById('films')
-       sidebar.appendChild(list)
        const deleteButton = document.createElement('button')
+       const deleteContainer=document.createElement('div')
+       deleteContainer.className='delContainer'
+       list.appendChild(deleteContainer)
        deleteButton.className='deleteFilm'
        deleteButton.innerHTML='type= button '
        deleteButton.innerText= "Delete Film"
-       list.appendChild(deleteButton)
-       deleteButton.addEventListener('click',removefilm(movie))
+       const sidebar=document.getElementById('films')
+       deleteContainer.appendChild(deleteButton)
+       sidebar.appendChild(list)
+       
+       deleteButton.addEventListener('click',()=>{
+        removefilm(movie.id)})
     });
-    
     }
-    
-    
 })
 function updateTickets(movieData){
-movieData.tickets_sold +=1
-console.log(movieData.tickets_sold)
-    fetch(`${url}/${movieData.id}`,
+     movieData.tickets_sold +=1
+     console.log(movieData.tickets_sold)
+     fetch(`${url}/${movieData.id}`,
     {
         method:'PATCH',
         headers:{
@@ -74,7 +83,20 @@ console.log(movieData.tickets_sold)
         body:JSON.stringify({tickets_sold: movieData.tickets_sold })
     }).then(res=>{res.json()}).then(data=> console.log(data))
 }
-function removefilm (movieData){
-    fetch(`${url}/${movi}`)
-}
+function removefilm(id){
+    fetch (`${url}/${id}`,{
+        method:'DELETE',
+        headers:{
+            'content-type':'application/json'
+        }
+    })
+    .then(res=>res.json()).then(data=>console.log(data))
+    }
+
+
+
+
+
+
+
 
